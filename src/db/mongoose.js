17eaff4bf32cb0179b362e1df1c028d05cebc783
TakeroxD <takeroxd@gmail.com>
2019-11-25 17:27:31 -0600

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 // revisa tu connectionURL aqui :-)
-var connectionURL = process.env.DATABASE_CONNECTION_STRING || require('../config.js').connectionURL
+const connectionURL = 'mongodb+srv://admin:admin@clustertaco-socc1.mongodb.net/exfinal?retryWrites=true&w=majority'
+//var connectionURL = process.env.DATABASE_CONNECTION_STRING || require('../config.js').connectionURL
 
 mongoose.connect( connectionURL, {
   useNewUrlParser: true,
